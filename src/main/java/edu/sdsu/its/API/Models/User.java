dc800892c3d5a60221dfcbed5c0f46a2dc791e5d
Tom Paulus <tom@tompaulus.com>
2017-01-27 16:20:40 -0800

    Added Some API Models.

---
+++ src/main/java/edu/sdsu/its/API/Models/User.java
@@ -0,0 +1,14 @@
+package edu.sdsu.its.API.Models;
+
+/**
+ * TODO JavaDoc
+ *
+ * @author Tom Paulus
+ *         Created on 1/27/17.
+ */
+public class User {
+    public int PK;
+    public String username;
+    private String password;
+    public String email;
+}
