8a1a116658066af83fc0f7ac3d51b677c2a150fe
Tom Paulus <tom@tompaulus.com>
2017-01-27 16:21:35 -0800

    Added Some Bb Learn API Models

---
+++ src/main/java/edu/sdsu/its/Blackboard/Models/Course.java
@@ -0,0 +1,13 @@
+package edu.sdsu.its.Blackboard.Models;
+
+/**
+ * TODO JavaDoc
+ *
+ * @author Tom Paulus
+ *         Created on 1/27/17.
+ */
+public class Course {
+    public String courseId;
+    public String name;
+    public boolean allowGuests = false;
+}
---
+++ src/main/java/edu/sdsu/its/Blackboard/Models/Enrollment.java
@@ -0,0 +1,13 @@
+package edu.sdsu.its.Blackboard.Models;
+
+/**
+ * TODO JavaDoc
+ *
+ * @author Tom Paulus
+ *         Created on 1/27/17.
+ */
+public class Enrollment {
+    public User user;
+    public Course childCourse;
+    public String courseRoleId;
+}
